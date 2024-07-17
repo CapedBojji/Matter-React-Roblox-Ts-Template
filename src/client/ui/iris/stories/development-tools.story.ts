1389587984b5a114305f02a66fbf206c4faa4e18
capedbojji <capedbojji@gmail.com>
2024-07-17 15:26:07 +0000

    Jul 17, 2024, 11:26 AM

--- src/client/ui/iris/stories/development-tools.story.ts
+++
@@ -1,9 +0,0 @@
-import { developmentTools } from "../components/development-tools";
-import { createIrisStory } from "../functions/create-story";
-
-export = async function (parent: GuiObject) {
-	return createIrisStory({
-		component: developmentTools,
-		parent,
-	});
-};
